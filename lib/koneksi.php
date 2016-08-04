92b64e6b254885dfe73022a3ecf3af39ad183914
zainul-haris <zainulharissaintek@gmail.com>
2016-08-04 10:50:09 +0700

    koneksi bro

---
+++ lib/koneksi.php
@@ -0,0 +1,10 @@
+<?php
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "db_crud";
+
+	// buat koneksi
+	$koneksi = mysqli_connect($servername, $username, $password, $dbname);
+
+?>
