f3b29109b99fdfa116cf32903c04819dda2a8733
zainul-haris <zainulharissaintek@gmail.com>
2016-08-04 10:51:11 +0700

    just commit bray

---
+++ module/header.php
@@ -0,0 +1,17 @@
+<meta charset ="UTF-8">
+	<meta name="viewport"	content="width=device-width, 	initial-scale=1">
+	<title>CRUD dengan PHP MYSQLI dan Bootstrap</title>
+
+<!-- load Boostrap css -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+
+<!-- load font awesome -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+
+	<style type="text/css">
+		body{
+			padding-top: 40px;
+			padding-bottom: 40px;
+		}
+
+	</style>
---
+++ module/navbar.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-inverse ">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#"><span class="glyphicon glyphicon-home"> CRUD</span></a>
+    </div>
+    <div>
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="index.php">Home</a></li>
+        <li><a href="data.php">Data</a></li>    
+      </ul>
+    </div>
+  </div>
+</nav>
